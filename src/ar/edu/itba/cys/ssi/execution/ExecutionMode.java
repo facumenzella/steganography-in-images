d7427d85c46da16d766637f8edd0cc9c79a7b3ec
Facundo Menzella <fmenzella@monits.com>
2015-06-05 19:12:23 -0300

    execution parameters interface addec

--- src/ar/edu/itba/cys/ssi/execution/ExecutionMode.java
+++ src/ar/edu/itba/cys/ssi/execution/ExecutionMode.java
@@ -4,5 +4,9 @@ public interface ExecutionMode {
 	
 	public void execute();
 	public void validateParameters() throws IllegalArgumentException;
+
 	
+	public interface ExecutionParameters {
+		// Just to make it more generic, but we will cast it where we need it	
+	}
 }
