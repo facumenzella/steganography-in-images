31ace350dbb0a865bfa2ec789d49c2dc14017c19
Facundo Menzella <fmenzella@monits.com>
2015-06-05 19:12:43 -0300

    distribution parameters added

---
+++ src/ar/edu/itba/cys/ssi/execution/DistributionParameters.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.cys.ssi.execution;
+
+import ar.edu.itba.cys.ssi.execution.ExecutionMode.ExecutionParameters;
+
+public class DistributionParameters implements ExecutionParameters{
+	
+	public DistributionParameters(final SSICmdExecutionParameters parameters) {
+		
+	}
+	
+}
