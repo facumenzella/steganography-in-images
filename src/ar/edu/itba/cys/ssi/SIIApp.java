8434f25f3f6fdab8f261b8b17bb38a2837fe0d03
Facundo Menzella <fmenzella@monits.com>
2015-06-04 23:20:15 -0300

    Distribute execution mode created and added to main

--- src/ar/edu/itba/cys/ssi/SIIApp.java
+++ src/ar/edu/itba/cys/ssi/SIIApp.java
@@ -1,5 +1,10 @@
 package ar.edu.itba.cys.ssi;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import ar.edu.itba.cys.ssi.execution.Distribute;
+import ar.edu.itba.cys.ssi.execution.ExecutionMode;
 import ar.edu.itba.cys.ssi.parser.SSICmdParser;
 import ar.edu.itba.cys.ssi.parser.SSICmdParser.SSICmdExecutionParameters;
 
@@ -11,13 +16,15 @@ public class SIIApp {
 		SSICmdParser parser = new SSICmdParser();
 		
 		SSICmdExecutionParameters parameters = parser.parse(args);
-		
+		ExecutionMode mode;
 		switch (parameters.getMode()) {
 		case RECOVER:
 			// We are recovering an image
 			break;
 		case DISTRIBUTE:
 			// We are distributing an image
+			// TODO change this when we know what we actually need
+			mode = new Distribute(parameters.getK(), parameters.getN(), "TODO put Image");
 			break;
 		default:
 			System.out.println("Holy shit the impossible hapenned");
--- src/ar/edu/itba/cys/ssi/execution/Distribute.java
+++ src/ar/edu/itba/cys/ssi/execution/Distribute.java
@@ -1,11 +1,39 @@
 package ar.edu.itba.cys.ssi.execution;
 
-public class Distribute implements ExecutionModes {
+import java.awt.Image;
 
+public class Distribute implements ExecutionMode {
+
+	private final int k;
+	private final int n;
+	private final String image; // TODO change this when we know what we actually need
+	
+	public Distribute(final int k, final int n, final String image) {
+		this.k = k;
+		this.n = n;
+		this.image = image;
+	}
+	
+	
 	@Override
 	public void execute() {
 		
-
+		
 	}
-
+	
+	public void validateParameters() throws IllegalArgumentException {
+		String msg = null;
+		if (k < 2) {
+			msg = "k must be greater or equal than 2";
+		} else if (k > n) {
+			msg = "k must be lower or equal than n";
+		} else if (n < 2) {
+			msg = "n must be at least 2";
+		} 
+		// TODO validate Image formatt
+		if (msg != null) {
+			throw new IllegalArgumentException(msg);
+		}
+	}
+	
 }
