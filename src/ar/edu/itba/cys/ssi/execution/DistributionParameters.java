fee653d46b204152626ae77750df641e8ffcc901
Facundo Menzella <fmenzella@monits.com>
2015-06-05 19:23:12 -0300

    removed distribution parameters file. now its an inner class from Distribute

--- src/ar/edu/itba/cys/ssi/execution/DistributionParameters.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.itba.cys.ssi.execution;
-
-import ar.edu.itba.cys.ssi.execution.ExecutionMode.ExecutionParameters;
-import ar.edu.itba.cys.ssi.parser.SSICmdParser.SSICmdExecutionParameters;
-
-
