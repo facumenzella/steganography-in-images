f746fe408bd7fd67988ae38b52698269b0add150
Facundo Menzella <fmenzella@monits.com>
2015-06-07 14:49:32 -0300

    mode added to parameters

---
+++ src/includes/parameters.h
@@ -0,0 +1,6 @@
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+typedef enum {DISTRIBUTE, RECOVER} mode;
+
+#endif
--- src/sii.c
+++ src/sii.c
@@ -1,5 +1,6 @@
 // includes
 #include "../includes/sii.h"
+#include "../includes/constants.h"
 
 int
 main(int argc, char * argv[]) {
