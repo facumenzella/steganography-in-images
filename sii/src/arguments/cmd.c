71878811d936b935ae4f6a5103687f81c000842e
Facundo Menzella <fmenzella@monits.com>
2015-06-20 11:44:28 -0300

    directory readed when its reveal mode

--- sii/src/arguments/cmd.c
+++ sii/src/arguments/cmd.c
@@ -16,27 +16,27 @@ boolean isValidWithinRelatedArguemtents(Arguments arguments);
 
 Arguments
 validateArguments(int argc, char *argv[], arguments_error *error) {
-  int ARGS = argc - 1; // we do not need the first argument
+    int ARGS = argc - 1; // we do not need the first argument
     boolean nWasProvided = FALSE;
-  if(invalidAmountOfArgs(ARGS)) {
-      setError(error, QTY_ARGS_ERROR);
-      return NULL;
-  }
+    if(invalidAmountOfArgs(ARGS)) {
+        setError(error, QTY_ARGS_ERROR);
+        return NULL;
+    }
     Arguments arguments = newArguments();
     
     printf("\n%s\n", "Starting to read console arguments...");
     setMode(arguments, validateModeType(argv[1], error));
     if (*error != NULL) {
-		return NULL;
-	}
+        return NULL;
+    }
     setSecret(arguments , validateSecret(argv[2], argv[3], error));
-	if (*error != NULL) {
-		return NULL;
-	}
+    if (*error != NULL) {
+        return NULL;
+    }
     setMinShadowsToRecoverSecret(arguments, validateMinShadowsToRecoverSecret(argv[4], argv[5], error));
     if (*error != NULL) {
-		return NULL;
-	}
+        return NULL;
+    }
     if (haveOptionalArguments(ARGS, 5)) {
         printf("%s\n", "Starting to read optional console arguments...");
         if (getMode(arguments) == DISTRIBUTE) {
@@ -53,10 +53,13 @@ validateArguments(int argc, char *argv[], arguments_error *error) {
                 if (haveOptionalArguments(ARGS, 7) == TRUE) {
                     setDirectory(arguments, validateDir(argv[8], argv[9], error));
                 }
-            // Maybe they have only provided dir
+                // Maybe they have only provided dir
             } else if (haveOptionalArguments(ARGS, 5) == TRUE) {
+                
                 setDirectory(arguments, validateDir(argv[6], argv[7], error));
             }
+        } else if (haveOptionalArguments(ARGS, 5) == TRUE) {
+            setDirectory(arguments, validateDir(argv[6], argv[7], error));
         }
     }
     // no dir, so we set the default
@@ -64,9 +67,9 @@ validateArguments(int argc, char *argv[], arguments_error *error) {
         setDirectory(arguments, defaultDir());
     }
     if (isValidWithinRelatedArguemtents(arguments) == FALSE) {
-		setError(error, K_GREATER_THAN_N);
-		return NULL;
-	}
+        setError(error, K_GREATER_THAN_N);
+        return NULL;
+    }
     
     if (nWasProvided == FALSE) {
         // we need to count the amount of shadows from the directory
@@ -74,91 +77,91 @@ validateArguments(int argc, char *argv[], arguments_error *error) {
         setTotalAmountOfShadowsToDistributeSecret(arguments,
                                                   countImagesInDirectory(getDirectory(arguments), error));
     }
-	return arguments;
+    return arguments;
 }
 
 boolean
 invalidAmountOfArgs(int args) {
-  boolean even = (args % 2 == 0) ? TRUE : FALSE;
-  if (args < MIN_REQUIRED_ARGS || args > MAX_ARGS || even) {
-    return TRUE;
-  }
-  return FALSE;
+    boolean even = (args % 2 == 0) ? TRUE : FALSE;
+    if (args < MIN_REQUIRED_ARGS || args > MAX_ARGS || even) {
+        return TRUE;
+    }
+    return FALSE;
 }
 
 
 mode_type
 validateModeType(char *arg, arguments_error *error) {
-  mode_type mode;
-	if(strcmp(arg, S_DISTRIBUTE_ARG) == 0 || strcmp(arg, DISTRIBUTE_ARG) == 0) {
-		mode = DISTRIBUTE;
-	} else if(strcmp(arg, S_RECOVER_ARG) == 0 || strcmp(arg, RECOVER_ARG) == 0) {
-		mode = RECOVER;
-	} else {
-		setError(error, MODE_ARG_ERROR);
-	}
-  printf("Mode: %s\n", MODE(arg));
-	return mode;
+    mode_type mode;
+    if(strcmp(arg, S_DISTRIBUTE_ARG) == 0 || strcmp(arg, DISTRIBUTE_ARG) == 0) {
+        mode = DISTRIBUTE;
+    } else if(strcmp(arg, S_RECOVER_ARG) == 0 || strcmp(arg, RECOVER_ARG) == 0) {
+        mode = RECOVER;
+    } else {
+        setError(error, MODE_ARG_ERROR);
+    }
+    printf("Mode: %s\n", MODE(arg));
+    return mode;
 }
 
 char *
 validateSecret(char *arg, char *value, arguments_error *error) {
-	char *s = NULL;
-  if(strcmp(arg, S_SECRET_ARG) == 0 || strcmp(arg, SECRET_ARG) == 0) {
-    s = calloc(SECRET_MAX_LENGTH, sizeof(char));
-    strcpy(s, value);
-    printf("Secret: %s\n", s);
-  } else {
-		setError(error, SECRET_ARG_ERROR);
-  }
-	return s;
+    char *s = NULL;
+    if(strcmp(arg, S_SECRET_ARG) == 0 || strcmp(arg, SECRET_ARG) == 0) {
+        s = calloc(SECRET_MAX_LENGTH, sizeof(char));
+        strcpy(s, value);
+        printf("Secret: %s\n", s);
+    } else {
+        setError(error, SECRET_ARG_ERROR);
+    }
+    return s;
 }
 
 int
 validateMinShadowsToRecoverSecret(char *arg, char*value, arguments_error *error) {
-	char *s = NULL;
-	if(strcmp(arg, S_K_ARG) == 0 || strcmp(arg, K_ARG) == 0) {
-    s = calloc(K_MAX_LENGTH, sizeof(char));
-    strcpy(s, value);
-    printf("Min shadows: %s\n", s);
-  } else {
-		setError(error, K_ARG_ERROR);
-		return -1;
-  }
-	int k = -1;
-	if ((k = atoi(s)) < 2) {
-		setError(error, K_LOWER_THAN_2);
-		return -1;
-	}
-	return k;
+    char *s = NULL;
+    if(strcmp(arg, S_K_ARG) == 0 || strcmp(arg, K_ARG) == 0) {
+        s = calloc(K_MAX_LENGTH, sizeof(char));
+        strcpy(s, value);
+        printf("Min shadows: %s\n", s);
+    } else {
+        setError(error, K_ARG_ERROR);
+        return -1;
+    }
+    int k = -1;
+    if ((k = atoi(s)) < 2) {
+        setError(error, K_LOWER_THAN_2);
+        return -1;
+    }
+    return k;
 }
 
 boolean
 checkIfTotalAmountOfShadowsWasProvided(char *arg, char*value, arguments_error *error) {
-  if(strcmp(arg, S_N_ARG) == 0 || strcmp(arg, N_ARG) == 0) {
-    return TRUE;
-  }
-  return FALSE;
+    if(strcmp(arg, S_N_ARG) == 0 || strcmp(arg, N_ARG) == 0) {
+        return TRUE;
+    }
+    return FALSE;
 }
 
 int
 validateTotalAmountOfShadowsToDistributeSecret(char *totalAmount, char *totalShadows, arguments_error *error) {
-	// there is no validation because we have already validated
-	char *s = calloc(N_MAX_LENGTH, sizeof(char));
-  strcpy(s, totalShadows);
-  printf("Total shadows: %s\n", s);
-	int n = -1;
-	if ( (n = atoi(s)) < 2) {
-		setError(error, N_LOWER_THAN_2);
-		return -1;
-	}
-	return n;
+    // there is no validation because we have already validated
+    char *s = calloc(N_MAX_LENGTH, sizeof(char));
+    strcpy(s, totalShadows);
+    printf("Total shadows: %s\n", s);
+    int n = -1;
+    if ( (n = atoi(s)) < 2) {
+        setError(error, N_LOWER_THAN_2);
+        return -1;
+    }
+    return n;
 }
 
 char*
 defaultDir() {
-	char *s = calloc(strlen(DIRECTORY_DEFAULT_VALUE), sizeof(char));
-	strcpy(s, DIRECTORY_DEFAULT_VALUE);
+    char *s = calloc(strlen(DIRECTORY_DEFAULT_VALUE), sizeof(char));
+    strcpy(s, DIRECTORY_DEFAULT_VALUE);
     return s;
 }
 
@@ -176,18 +179,18 @@ validateDir(char *arg, char*value, arguments_error *error) {
 
 boolean
 haveOptionalArguments(int argc, int lastReaded) {
-  if (argc > lastReaded) {
-    return TRUE;
-  }
-  return FALSE;
+    if (argc > lastReaded) {
+        return TRUE;
+    }
+    return FALSE;
 }
 
 boolean
 isValidWithinRelatedArguemtents(Arguments arguments) {
-	// k <= n
-	if ( (getTotalAmountOfShadowsToDistributeSecret(arguments) == -1) ||
+    // k <= n
+    if ( (getTotalAmountOfShadowsToDistributeSecret(arguments) == -1) ||
         getMinShadowsToRecoverSecret(arguments) <= getTotalAmountOfShadowsToDistributeSecret(arguments)) {
-		return TRUE;
-	}
-	return FALSE;
+        return TRUE;
+    }
+    return FALSE;
 }
