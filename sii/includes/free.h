fa074f21689e2226811db914dba8afb1934f4bd6
Facundo Menzella <fmenzella@monits.com>
2015-06-21 23:28:09 -0300

    moved files to src

--- sii/includes/free.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FREE_H__
-#define __FREE_H__
-
-void freeSquareMatrix(void** matrix, int dimension);
-
-#endif
--- sii/includes/random.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RANDOM_H__
-#define __RANDOM_H__
-
-#import <stdlib.h>
-
-long int getRandom(long int max);
-void randomize(int num);
-
-#endif
